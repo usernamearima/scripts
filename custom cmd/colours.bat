e9199a12edc7011851f474e6e04ea2cbae931aba
username <oskarszandar55@gmail.com>
2026-02-16 16:21:20 +0100

    Add ANSI color demo batch file
    This batch file demonstrates various ANSI colors and text styles in the command prompt.

---
+++ custom cmd/colours.bat
@@ -0,0 +1,56 @@
+@echo off
+cls
+
+echo ==============================
+echo        ANSI COLOR DEMO
+echo ==============================
+echo.
+
+echo [ Bold colors ]
+echo [1;90mGrey[0m
+echo [1;91mRed[0m
+echo [1;92mGreen[0m
+echo [1;93mYellow[0m
+echo [1;94mBlue[0m
+echo [1;95mMagenta[0m
+echo [1;96mCyan[0m
+echo [1;97mWhite[0m
+echo.
+
+echo [ Normal colors ]
+echo [90mGrey[0m
+echo [91mRed[0m
+echo [92mGreen[0m
+echo [93mYellow[0m
+echo [94mBlue[0m
+echo [95mMagenta[0m
+echo [96mCyan[0m
+echo [97mWhite[0m
+echo.
+
+echo [ Text styles ]
+echo [4mUnderlined[0m
+echo [7mHighlighted[0m
+echo.
+
+echo [ Dark colors ]
+echo [30mBlack[0m
+echo [31mDark Red[0m
+echo [32mDark Green[0m
+echo [33mDark Yellow[0m
+echo [34mDark Blue[0m
+echo [35mDark Purple[0m
+echo [36mDark Cyan[0m
+echo.
+
+echo [ Background colors ]
+echo [41mRed Background[0m
+echo [42mGreen Background[0m
+echo [43mYellow Background[0m
+echo [44mBlue Background[0m
+echo [45mPurple Background[0m
+echo [46mCyan Background[0m
+echo [47mWhite Background[0m
+echo.
+
+pause
